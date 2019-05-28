c6144895e60b7e0c74d63210d84808254cc3f5eb
dingdong11 <1371682092@qq.com>
2019-05-28 09:42:59 +0800

    adfasd

--- pool.js
+++ pool.js
@@ -1,11 +1,16 @@
 const mysql=require("mysql");
 //创建连接池
 const pool = mysql.createPool({
-    host:"127.0.0.1",
-    port:"3306",
-    user:"root",
-    password:"",
-    database:"mobile",
-	connectionLimit:20
+    // host:"127.0.0.1",
+    // port:"3306",
+    // user:"root",
+    // password:"",
+    // database:"mobile",
+    // connectionLimit:20
+    host : process.env.MYSQL_HOST, 
+ port : process.env.MYSQL_PORT, 
+ user : process.env.ACCESSKEY, 
+ password : process.env.SECRETKEY, 
+ database : 'app_' + process.env.APPNAME 
 });
 module.exports=pool
